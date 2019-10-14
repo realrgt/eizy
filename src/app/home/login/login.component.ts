ec53821f92b7601a7ebedff7693c805a429db183
realrgt <ergitorubildo@gmail.com>
2019-10-14 02:24:46 +0200

    auth-content

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,8 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
-import { Router } from '@angular/router';
-import { User } from 'src/app/core/user';
-import { UserService } from 'src/app/core/user.service';
+// import { Component, OnInit } from '@angular/core';
+// import { FormGroup, FormBuilder } from '@angular/forms';
+// import { Router } from '@angular/router';
+// import { User } from 'src/app/core/user';
+// import { UserService } from 'src/app/core/user.service';
+
+// @Component({
+//   selector: 'app-login',
+//   templateUrl: './login.component.html',
+//   styleUrls: ['./login.component.scss']
+// })
+// export class LoginComponent implements OnInit {
+
+//   users: User[];
+//   form: FormGroup;
+//   hide = true;
+
+//   constructor(
+//     private userService: UserService,
+//     private fb: FormBuilder,
+//     private router: Router
+//   ) { }
+
+//   ngOnInit() {
+
+//     this.form = this.fb.group({
+//       id: [null],
+//       username: [null],
+//       password: [null]
+//     });
+
+//   }
+
+//   login() {
+//     this.router.navigate(['employees']);
+//   }
+
+// }
+
+import { OnInit, Component } from '@angular/core';
+import { AuthLoginInfo } from 'src/app/core/auth/login-info';
+import { AuthService } from 'src/app/core/auth/auth.service';
+import { TokenStorageService } from 'src/app/core/auth/token-storage.service';
 
 @Component({
   selector: 'app-login',
@@ -10,29 +48,49 @@ import { UserService } from 'src/app/core/user.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  form: any = {};
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = '';
+  roles: string[] = [];
+  private loginInfo: AuthLoginInfo;
 
-  users: User[];
-  form: FormGroup;
-  hide = true;
-
-  constructor(
-    private userService: UserService,
-    private fb: FormBuilder,
-    private router: Router
-  ) { }
+  constructor(private authService: AuthService, private tokenStorage: TokenStorageService) { }
 
   ngOnInit() {
-
-    this.form = this.fb.group({
-      id: [null],
-      username: [null],
-      password: [null]
-    });
-
+    if (this.tokenStorage.getToken()) {
+      this.isLoggedIn = true;
+      this.roles = this.tokenStorage.getAuthorities();
+    }
   }
 
-  login() {
-    this.router.navigate(['employees']);
+  onSubmit() {
+    console.log(this.form);
+
+    this.loginInfo = new AuthLoginInfo(
+      this.form.username,
+      this.form.password);
+
+    this.authService.attemptAuth(this.loginInfo).subscribe(
+      data => {
+        this.tokenStorage.saveToken(data.accessToken);
+        this.tokenStorage.saveUsername(data.username);
+        this.tokenStorage.saveAuthorities(data.authorities);
+
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        this.roles = this.tokenStorage.getAuthorities();
+        this.reloadPage();
+      },
+      error => {
+        console.log(error);
+        this.errorMessage = error.error.message;
+        this.isLoginFailed = true;
+      }
+    );
   }
 
+  reloadPage() {
+    window.location.reload();
+  }
 }
